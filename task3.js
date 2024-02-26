c4f3c3fccb9de116a8d6495396c631510bc01067
Maneeeeee <143323531+Maneeeeee@users.noreply.github.com>
2024-02-27 00:43:49 +0400

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,21 @@
+var div = 0;
+
+
+function changeTheButton()
+{
+    if(div == 1)
+    {
+        document.querySelector("#loginButton").textContent = "The button is clicked";
+        document.querySelector("#state").textContent = "Thank you";
+        
+        div = 0;
+    }
+    else 
+    {
+        document.querySelector("#loginButton").textContent = "The button is not clicked";
+        document.querySelector("#state").textContent = "Click the button"; 
+
+        div = 1;
+    }
+}
+document.getElementById("loginButton").addEventListener("click", changeTheButton);
