e52768f11536466c28b87300932466e7f0b32a09
Maneeeeee <143323531+Maneeeeee@users.noreply.github.com>
2024-02-29 00:32:21 +0400

    task4.js

---
+++ task4.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.innerHTML === "Is not clicked!") {
+      x.innerHTML = "Is clicked!";
+    } else {
+      x.innerHTML = "Is not clicked!";
+    }
+  }
+ 
